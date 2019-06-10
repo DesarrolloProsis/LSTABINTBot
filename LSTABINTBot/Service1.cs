f0c56ebf08126dc586daa1952cef9f196c1701b6
Miguel Ramirez <mike__221@hotmail.com>
2019-06-10 13:23:44 -0500

    Recibe datos de WebApiGuate, aún sin lógica

--- LSTABINTBot/Service1.cs
+++ LSTABINTBot/Service1.cs
@@ -4,25 +4,61 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
 
 namespace LSTABINTBot
 {
     public partial class Service1 : ServiceBase
     {
+        Timer timer = new Timer();
         public Service1()
         {
             InitializeComponent();
         }
 
+        public void Inicio()
+        {
+
+        }
         protected override void OnStart(string[] args)
         {
         }
 
         protected override void OnStop()
         {
+        }
+        public async void CheckListas()
+        {
+            using (var client = new HttpClient())
+            {
+                var GetLastLista = await client.PostAsync(new Uri("http://localhost:87/Home/GetListas"), null);
+
+                var ReceiveLastLista = await client.PostAsync(new Uri("http://localhost:87/Home/SendListas"),null);
+
+                var FechaLastLista = await ReceiveLastLista.Content.ReadAsStringAsync();
+            }
+        }
+        public async void CheckServiceTags()
+        {
+            using (var client = new HttpClient())
+            {
+                var GetOracleCount = await client.PostAsync(new Uri("http://localhost:87/Home/GetOracleTransactions"), null);
+                var GetSQLCount = await client.PostAsync(new Uri("http://localhost:87/Home/GetSQLTransactions"), null);
+
+                var ReceiveOracleCount = await client.PostAsync(new Uri("http://localhost:87/Home/SendOracleTransactions"),null);
+                var ReceiveSQLCount = await client.PostAsync(new Uri("http://localhost:87/Home/SendSQLTransactions"), null);
+
+                var OracleCount = await ReceiveOracleCount.Content.ReadAsStringAsync();
+                var SQLCount = await ReceiveSQLCount.Content.ReadAsStringAsync();
+            }
         }
     }
 }
---
+++ TestLSTABINTBot/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestLSTABINTBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Inicio_Click(object sender, EventArgs e)
+        {
+            LSTABINTBot.Service1 service = new LSTABINTBot.Service1();
+            service.CheckListas();
+        }
+    }
+}
